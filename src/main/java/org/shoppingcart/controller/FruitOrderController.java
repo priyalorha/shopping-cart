45955e4746fe89dd86109fd54f89742c74eda3b3
Priya <priya.lorha@gmail.com>
2025-08-14 12:54:42 +0530

    shopping cart

--- src/main/java/org/shoppingcart/controller/FruitOrderController.java
+++ src/main/java/org/shoppingcart/controller/FruitOrderController.java
@@ -2,9 +2,11 @@ package org.shoppingcart.controller;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
+import org.shoppingcart.models.Fruit;
 import org.shoppingcart.models.FruitDTO;
 import org.shoppingcart.models.FruitResponseDTO;
 import org.shoppingcart.services.FruitBillService;
+import org.shoppingcart.utils.FruitType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +15,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
@@ -31,10 +36,22 @@ public class FruitOrderController {
     @PostMapping("/bill")
     public ResponseEntity<FruitResponseDTO> computeBill(
             @Valid @NotEmpty(message = "Fruit list cannot be empty")
-            @RequestBody List<FruitDTO> fruitDTO) throws IOException {
+            @RequestBody List<FruitType> fruits) throws IOException {
 
-        log.info("Received bill request for {} items", fruitDTO.size());
-        FruitResponseDTO fruitBill = fruitCalculatorService.billCalculator(fruitDTO);
+
+        Map<FruitType, Long> fruitCount = fruits.stream()
+                .collect(Collectors.groupingBy(f -> f, Collectors.counting()));
+
+        // Convert to List<FruitDTO>
+        List<FruitDTO> fruitDTOList = fruitCount.entrySet().stream()
+                .map(entry -> new FruitDTO(entry.getKey(), entry.getValue().intValue()))
+                .toList();
+
+
+        List<FruitDTO> fruitDTO = new ArrayList<>();
+
+        log.info("Received bill request for {} items", fruitDTOList.size());
+        FruitResponseDTO fruitBill = fruitCalculatorService.billCalculator(fruitDTOList);
         log.info("Bill computed successfully: {}", fruitBill);
 
         return ResponseEntity.status(HttpStatus.OK).body(fruitBill);
